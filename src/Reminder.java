23c51602dd3987dc87626f452108bedca0e7c8fd
samancro <samanamp@outlook.com>
2013-09-25 22:24:36 +1000

    Minor change

--- src/Reminder.java
+++ src/Reminder.java
@@ -29,7 +29,7 @@ public class Reminder extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request,
+	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		JSONObject res = appLogic(request);
@@ -78,7 +78,7 @@ public class Reminder extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request,
+	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
